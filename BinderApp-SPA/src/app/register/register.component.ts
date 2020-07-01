34fc350fc52882dda78b867ad7250197b80bfd21
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-07-01 18:59:22 +0300

    Added register user functionality in the SPA. Added global exception handler in the API proj with a helper extension method

--- BinderApp-SPA/src/app/register/register.component.ts
+++ BinderApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,21 +7,23 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
  model: any = {};
-  constructor() { }
+  constructor(private authservice: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authservice.register(this.model).subscribe(() => {
+          console.log('registration successfull!'); },
+               error => {
+                  console.log(error);
+              });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
   }
-
 
 }
--- BinderApp.API/Controllers/AuthController.cs
+++ BinderApp.API/Controllers/AuthController.cs
@@ -48,6 +48,8 @@ namespace BinderApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Fatal Errrorrrr!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
