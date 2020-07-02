2e835d9b04cf5df1df5743cfa67022b999732b0c
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-07-02 13:39:30 +0300

    Removed deliberate exception, updated gitignore file

--- BinderApp.API/Controllers/AuthController.cs
+++ BinderApp.API/Controllers/AuthController.cs
@@ -48,8 +48,6 @@ namespace BinderApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Fatal Errrorrrr!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
