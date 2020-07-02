dd4a33e4b111e07cfba72280cfe63cf96ca3c4ae
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-07-02 16:08:00 +0300

    Imolemented Alertify external library for client notification

--- BinderApp-SPA/src/app/_services/error.interceptor.ts
+++ BinderApp-SPA/src/app/_services/error.interceptor.ts
@@ -28,7 +28,7 @@ export class ErrorInterceptor implements HttpInterceptor{
                     return throwError(modelStateErrors || serverError || 'Server Error');
                 }
             })
-        )
+        );
     }
 }
 
