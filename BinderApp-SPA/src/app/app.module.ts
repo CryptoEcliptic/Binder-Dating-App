f9fa842d6130c14083b976e90caee1c1d88a12f9
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-07-24 14:04:10 +0300

    Added photo editor component

--- BinderApp-SPA/src/app/app.module.ts
+++ BinderApp-SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 export function tokenGetter(){
    return localStorage.getItem('token');
@@ -45,7 +46,9 @@ export function tokenGetter(){
       MemberDetailComponent,
       MemberEditComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      PhotoEditorComponent
+
    ],
    imports: [
       BrowserModule,
