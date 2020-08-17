0a41fcd94456b7618a2f20e1d4dc903324309739
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-08-17 12:07:37 +0300

    Finished pagination functionality in the API

--- BinderApp.API/Data/IDatingRepository.cs
+++ BinderApp.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using BinderApp.API.Helpers;
 using BinderApp.API.Models;
 
 namespace BinderApp.API.Data
@@ -12,7 +13,7 @@ namespace BinderApp.API.Data
 
          Task<bool> SaveAll();
 
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
 
          Task<User> GetUser(int id);
 
