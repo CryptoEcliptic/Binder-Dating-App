92ff6469c44b42f8898ccf6c07c4a6e8f6ec79cb
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-08-18 12:12:08 +0300

    Added user filtering functionality in the API

--- BinderApp.API/Helpers/UserParams.cs
+++ BinderApp.API/Helpers/UserParams.cs
@@ -20,6 +20,14 @@ namespace BinderApp.API.Helpers
               pageSize = (value > MaxPageSize) ? MaxPageSize : value;
             }
         }
+
+        public int UserId { get; set; }
+
+        public string Gender { get; set; }
+
+        public int MinAge { get; set; } = 18;
+
+        public int MaxAge { get; set; } = 99;
         
     }
 }
