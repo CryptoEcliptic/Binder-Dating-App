863ceb8213942d191bcfeceb70d1df1b14d86c50
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-09-28 14:52:11 +0300

    Added the design of the message tab system

--- BinderApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ BinderApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -7,7 +7,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
-  styleUrls: ['./member-messages.component.scss']
+  styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
 @Input() recipientId: number;
