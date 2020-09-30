e90f0f425e119edddf26a40e1ed4cc12356fd0f3
CryptoCryptoEcliptic <r.e.vassilev@gmail.com>
2020-09-30 13:43:39 +0300

    Added Mark message as read functionality

--- BinderApp.API/Controllers/MessagesController.cs
+++ BinderApp.API/Controllers/MessagesController.cs
@@ -143,5 +143,29 @@ namespace BinderApp.API.Controllers
             throw new Exception("Error deleting the message!");
         }
 
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var message = await _repo.GetMessage(id);
+
+            if(message.RecipientId != userId)
+            {
+                return Unauthorized();
+            }
+
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+        }
+
+
     }
 }
